f1cfa4a51c6a0ffcc6dfc6e8afd72072ee25f9b4
Balal Arast <arastbalal@gmail.com>
2022-07-05 15:41:06 +0430

    update new

---
+++ nuxt/components/lib/vue2/test.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="custom-counter">
+    HemendTest
+  </div>
+</template>
+
+<script>
+// components/lib/test.vue
+export default {
+  computed: {
+    pluginOptions() {
+      return this.options || {}
+    },
+    injectedPluginName() {
+      const { pluginOptions } = this
+      return pluginOptions.namespace ? '$' + pluginOptions.namespace : undefined
+    }
+  }
+}
+</script>
+
